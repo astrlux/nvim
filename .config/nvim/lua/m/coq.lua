da461f1f9346393344b0387ae707f85615d04920
astrlux <37908451+astrlux@users.noreply.github.com>
2022-03-14 02:22:26 -0700

    remove _dep

--- .config/nvim/lua/m/coq.lua
+++ .config/nvim/lua/m/coq.lua
@@ -1,5 +1,12 @@
+-- speed, minimalism, low profile
 vim.g.coq_settings = {
-  auto_start = 'shut-up'
+  auto_start = 'shut-up',
+  display = {
+    icons = {
+      mode = 'none'
+    },
+    pum = {
+      fast_close = false
+    }
+  }
 }
-
-require 'coq'
