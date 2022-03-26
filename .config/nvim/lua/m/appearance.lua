04b4fba93c170785e59383144983c00743dd0de3
astrlux <37908451+astrlux@users.noreply.github.com>
2022-03-26 10:22:42 -0700

    post_obfuscation + allow cs

--- .config/nvim/lua/m/appearance.lua
+++ .config/nvim/lua/m/appearance.lua
@@ -1,9 +1,9 @@
 local o, g, cmd = vim.opt, vim.g, vim.cmd
 o.tgc = true
 o.bg = 'dark'
-cmd 'colorscheme monochrome'
-vim.api.nvim_exec([[
-hi Normal guibg=none
-hi LspFloatWinBorder guifg=white guibg=none
-hi EndOfBuffer guifg=#444444
-]], false)
+cmd 'colorscheme karonda'
+-- vim.api.nvim_exec([[
+-- hi LspFloatWinBorder guifg=white guibg=none
+-- hi Normal guibg=none
+-- hi EndOfBuffer guifg=#444444
+-- ]], false)
